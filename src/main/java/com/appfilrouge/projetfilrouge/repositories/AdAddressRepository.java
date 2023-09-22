a7415503efd162b4f940d22d1912bdd4d4dbda2e
ArnaudDeploiement <127393720+ArnaudDeploiement@users.noreply.github.com>
2023-09-22 20:51:06 +0200

    version5

--- src/main/java/com/appfilrouge/projetfilrouge/repositories/AdAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.appfilrouge.projetfilrouge.repositories;
-
-import com.appfilrouge.projetfilrouge.entities.AdAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdAddressRepository extends JpaRepository<AdAddress, Long> {
-}
--- src/main/java/com/appfilrouge/projetfilrouge/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.appfilrouge.projetfilrouge.repositories;
-
-import com.appfilrouge.projetfilrouge.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
